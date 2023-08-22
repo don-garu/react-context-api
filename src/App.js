77bd56917f7112cb255e91814801c3d7dbc9ca4e
don-garu <hwangji9637@gmail.com>
2023-08-23 08:55:32 +0900

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from "react";
-import "./style.css";
+import React, { createContext, useContext } from 'react';
+import './style.css';
+
+const themeDefault = { border: '10px solid green' };
+const themeContext = createContext(themeDefault);
+
+function Sub1() {
+  const theme = useContext(themeContext);
+  return (
+    <themeContext.Provider value={{ border: '10px solid red' }}>
+      <div style={theme}>
+        <h1>Sub1</h1>
+        <Sub2 />
+      </div>
+    </themeContext.Provider>
+  );
+}
+
+function Sub2() {
+  const theme = useContext(themeContext);
+  return (
+    <div style={theme}>
+      <h1>Sub2</h1>
+      <Sub3 />
+    </div>
+  );
+}
+
+function Sub3() {
+  const theme = useContext(themeContext);
+  return (
+    <div style={theme}>
+      <h1>Sub3</h1>
+    </div>
+  );
+}
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="root">
+      <h1>Hello World!</h1>
+      <Sub1 />
     </div>
   );
 }
